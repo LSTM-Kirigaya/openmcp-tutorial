bced6fa166b33cac525d03de90ff703991e54119
kirigaya <Kirigaya>
2025-05-25 23:02:03 +0800

    fix some bug

--- crawl4ai-mcp/main.py
+++ crawl4ai-mcp/main.py
@@ -1,15 +1,20 @@
 from crawl4ai import AsyncWebCrawler
 from mcp.server.fastmcp import FastMCP
+from crawl4ai.async_configs import BrowserConfig
 
 mcp = FastMCP("crawl4ai-mcp")
+
+# Using proxy URL
+browser_config = BrowserConfig(proxy="http://127.0.0.1:7890")
 
 @mcp.tool(
     name='get_web_markdown',
     description='获取网页的主体内容，并转换为Markdown格式。'
 )
 async def get_web_markdown(url: str) -> str:
-    async with AsyncWebCrawler() as crawler:
+    async with AsyncWebCrawler(config=browser_config) as crawler:
         result = await crawler.arun(
             url=url
         )
-        return result.markdown
+
+        return str(result.markdown)
